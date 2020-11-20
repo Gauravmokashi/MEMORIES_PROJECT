52a40de7834960b24e52c446a0a0ea171827d274
ML17 <mokashigaurav1@gmail.com>
2020-11-20 20:43:45 +0530

    react form created

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = expess();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://gaurav:gaurav123@cluster0.6s6m6.mongodb.net/<dbname>?retryWrites=true&w=majority";
